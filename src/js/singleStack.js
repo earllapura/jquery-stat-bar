386b577ac7ebbb1ebe2aba45199399c8179c3ef0
earllapura <earlvon.lapura@g.msuiit.edu.ph>
2016-08-05 17:33:26 +0800

    Added options for the object parameter for client customization

--- src/js/singleStack.js
+++ src/js/singleStack.js
@@ -1,36 +1,68 @@
 (function( $ ) {
-	$.fn.singleStack = function(data) {
-		var total = 0;
+	$.fn.singleStack = function(graphData) {
+		//defaults
+		var defaults = {
+			graph: {
+				className: '',
+				clickEvent: function(e){},
+				slices: {
+					className: ''
+				}
+			},
+
+			viewport: {
+				className: '',
+				listContainer: {
+					className: ''
+				}
+			},
+
+			series:{
+				clickEvent: function(e){},
+				data: []
+			}
+		};
+		//merge recursively with supplied data
+		$.extend(true, defaults, graphData);
+		//copy merged to data
+		graphData = defaults;
+		var total = 0,
+			viewportElement = $("."+graphData.viewport.className),
+			sliceClassNames = graphData.graph.slices.className;
+
 		//throw error if not div element
 		if(!(this.is('div'))) throw new Error('Element attached is not a div element.');
 		//warn if no viewport is available
-		if($('div.single-stack-bar-stats').length === 1){
-			$('div.single-stack-bar-stats').hide();
-			$('div.single-stack-bar-stats').append('<ol class="single-stack-bar-stats-numbers"></ol>');
-			this.click(function(){
-				$('div.single-stack-bar-stats').slideToggle('slow');
-			});
-		}
-		else console.warn('Warning: No viewport element is available');
+		if(viewportElement.length === 0) throw new Error('No viewport available');
 		//throw error if data is not array
-		if(!($.isArray(data))) throw new Error('Data inputted is not an array');
+		if(!($.isArray(graphData.series.data))) throw new Error('Data inputted is not an array');
 		//warn if data has no elements
-		if(!($.isArray(data))) console.warn('Array inputted is not an array');
+		if(graphData.series.length === 0) console.warn('Array inputted is not an array');
+
 		//add styles to div
 		this.addClass('single-stack-bar-container');
-		//return length
-		if(data.length === 0) return this;
+		this.addClass(graphData.graph.className);
+		viewportElement.addClass('single-stack-bar-stats');
+
+		//Add click event
+		this.click(graphData.graph.clickEvent);
+
+		//Add list container
+		viewportElement.append('<ol class="'+graphData.viewport.listContainer.className+'"></ol>');
+
 		//get total
-		for (var i = data.length - 1; i >= 0; i--) {
-			total += parseFloat(data[i].value);
-		}
-		//append the bar
-		for (var j = 0; j < data.length; j++) {
-			var percentage = ((data[j].value/total)*100).toFixed(2);
-			this.append('<span class="single-stack-slice" style="width:'+percentage+'%; background-color:'+data[j].color+';">'+data[j].name+'</span>');
-			$('div.single-stack-bar-stats ol').append('<li><a href="#visualization"><span class="legend-color" style="background-color:'+data[j].color+'"></span><span class="legend-name">'+data[j].name+'</span><span class="percent">'+percentage+'%</span></a></li>');
+		for (var i = graphData.series.data.length - 1; i >= 0; i--) {
+			total += parseFloat(graphData.series.data[i].y);
 		}
 
+		for (var j = 0; j < graphData.series.data.length; j++) {
+			var percentage = ((graphData.series.data[j].y/total)*100).toFixed(2);
+			//append to graph proper
+			this.append('<span class="single-stack-slice '+sliceClassNames+'" style="width:'+percentage+'%; background-color:'+graphData.series.data[j].color+';">'+graphData.series.data[j].name+'</span>');
+			// append to stat bar
+			viewportElement.find('ol').append('<li class="single-stack-legend-strip"><span class="single-stack-legend-color" style="background-color:'+graphData.series.data[j].color+'"></span><span class="single-stack-legend-name">'+graphData.series.data[j].name+'</span><span class="percent">'+percentage+'%</span></li>');
+		}
+		this.find('single-stack-legend-strip').click(graphData.series.clickEvent);
 		return this;
 	};
 }( jQuery ));
